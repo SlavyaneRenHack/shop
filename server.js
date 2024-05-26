e343e3c42f349181b066766bd50d22315fccb688
dmitryobukhoff <obukhov-da04@yandex.ru>
2024-05-26 09:21:16 +0300

    second commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 const axios = require('axios');
 
+
 // Переменные для хранения 
-let token = ""
 let amount = ""
 let nameshop = ""
 let payId = ""
@@ -14,6 +14,7 @@ const PORT = 8001
 
 // Установка статической папки
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Маршрут для главной страницы
 app.get('/shop', (req, res) => {
@@ -32,7 +33,6 @@ app.get('/pay', async (req, res) => {
 
     const response = await axios.post(url, jsonData); // Отправка POST запроса
     console.log('Response:', response.data);
-    token = response.data
     res.send(response.data); // Отправка ответа клиенту, чтобы не было бесконечного запроса
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
@@ -42,34 +42,43 @@ app.get('/pay', async (req, res) => {
 
 
 
-app.get('/client_pay', (req, res) => {
-  const url = 'http://renpay.nikcorp.ru/client_pay'
+app.get('/client_pay', async (req, res) => {
+  const url = 'http://renpay.nikcorp.ru/client_pay';
+  const token = req.query.token;
+  console.log("Token from /client_pay " + token);
+
   const jsonData = JSON.stringify({
-    token : token
-  })
+    token: token
+  });
+
   // Отправка GET-запроса с параметрами
   let config = {
     method: 'get',
     maxBodyLength: Infinity,
     url: url,
-    headers: { 
+    headers: {
       'Content-Type': 'application/json'
     },
-    data : jsonData
+    data: jsonData
   };
 
-  axios.request(config)
-  .then((response) => {
-    amount = response.data.amount
-    nameshop = response.data.nameshop
-    payId = response.data.payId
-    console.log(`Data Recieving: ${response.data.amount} ${response.data.nameshop} ${response.data.payId}`)
-    res.send(response.data) // Отправляем ответ, чтобы не было бесконечного запроса
-  })
-  .catch((error) => {
+  try {
+    const response = await axios.request(config);
+    const amount = response.data.amount;
+    const nameshop = response.data.nameshop;
+    const payId = response.data.payId;
+    console.log(`Data Receiving: ${amount} ${nameshop} ${payId}`);
+    
+    // Теперь, когда мы уверены, что все данные получены, мы можем безопасно перенаправить пользователя
+    res.send(response.data)
+  } catch (error) {
     console.log(error);
-  });
+    // Обработка ошибки, например, отправка ответа с кодом ошибки
+    res.status(500).send('Internal Server Error');
+  }
 });
+
+ 
 
 app.get('/pay_status', (req, res) => {
   const url = 'http://renpay.nikcorp.ru/pay_status'
